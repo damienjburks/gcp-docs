b8c3f1767ec4db79d5d70535e62ed6a194d93ae9
Damien Burks <damien@damienjburks.com>
2024-07-30 19:13:48 -0500

    testing

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# GCP Docusaurus
+# GCP Docusaurus Documentation Site
 
-This website is built using [Docusaurus](https://docusaurus.io/), a modern static website generator.
+![GitHub branch check runs](https://img.shields.io/github/check-runs/damienjburks/gcp-docusaurus/main)
+[![License](https://img.shields.io/github/license/damienjburks/gcp-docusaurus)](https://github.com/damienjburks/gcp-docusaurus/blob/main/LICENSE)
+[![Docusaurus](https://img.shields.io/badge/docs-docusaurus-5B9BD5)](https://docusaurus.io/)
+
+This project is a documentation site built with [Docusaurus](https://docusaurus.io/) and deployed using Google Cloud Build.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Development](#development)
+- [Build](#build)
+- [Deployment](#deployment)
+- [License](#license)
+
+## Installation
+
+To install the dependencies for this project, run:
+
+```sh
+npm install
+```
+
+## Development
+
+To start the development server, run:
+
+```sh
+npm run start
+```
+
+This will start the Docusaurus development server and open your site in a new browser tab. Most changes are reflected live without having to restart the server.
+
+## Build
+
+To build the static files for the documentation site, run:
+
+```sh
+npm run build
+```
+
+The static files will be generated in the `build` directory.
+
+## Deployment
+
+This project uses Google Cloud Build for continuous deployment. The build configuration is defined in the `cloudbuild.yaml` file.
+
+To deploy the site, ensure you have set up a Cloud Build trigger in your GCP project that watches for changes in the repository and runs the build steps defined in `cloudbuild.yaml`.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://dsb-innovation-hub.uc.r.appspot.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'DSB', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/damienjburks/gcp-docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/damienjburks/gcp-docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -72,7 +72,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/damienjburks/gcp-docusaurus',
           label: 'GitHub',
           position: 'right',
         },
@@ -116,7 +116,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/damienjburks/gcp-docusaurus',
             },
           ],
         },
